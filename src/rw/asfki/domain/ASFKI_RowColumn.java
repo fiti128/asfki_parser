210e16109739001f0a1f9de86123ac68440aa432
unknown <C:\Documents and Settings\ircm_yanusheusky\Application Data\The Bat!>
2013-01-29 08:07:28 +0300

    add to reader

---
+++ src/rw/asfki/domain/ASFKI_RowColumn.java
@@ -0,0 +1,21 @@
+package rw.asfki.domain;
+
+import java.util.Map;
+
+public class ASFKI_RowColumn {
+	private Map<String, String> attributes;
+	private String body;
+	public Map<String, String> getAttributes() {
+		return attributes;
+	}
+	public void setAttributes(Map<String, String> attributes) {
+		this.attributes = attributes;
+	}
+	public String getBody() {
+		return body;
+	}
+	public void setBody(String body) {
+		this.body = body;
+	}
+	
+}
